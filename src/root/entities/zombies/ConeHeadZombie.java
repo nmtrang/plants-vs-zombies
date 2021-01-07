4388e80c1b248891a0c68469ae45b223bba31364
MrCat-2510 <panpuu123@gmail.com>
2021-01-07 16:26:04 +0700

    Added zombies

---
+++ src/root/entities/zombies/ConeHeadZombie.java
@@ -0,0 +1,24 @@
+package root.entities.zombies;
+
+import java.awt.Image;
+
+public class ConeHeadZombie extends Zombie {
+
+    public ConeHeadZombie(int x, int y) {
+        super(x, y);
+        health = 250;
+    }
+
+    @Override
+    public Image getImage() {
+        if (collided) {
+            if (health >= 150) return visualMode.ConeHeadZombieEatImage;
+            else if (health >= 60) return visualMode.NormalZombieEatImage;
+            else return visualMode.NormalZombieLostHeadEatImage;
+        } else {
+            if (health >= 150) return visualMode.ConeHeadZombieWalkImage;
+            else if (health >= 60) return visualMode.NormalZombieWalkImage;
+            else return visualMode.NormalZombieLostHeadWalkImage;
+        }
+    }
+}
---
+++ src/root/entities/zombies/Zombie.java
@@ -0,0 +1,81 @@
+package root.entities.zombies;
+
+import java.awt.Point;
+import javax.swing.JLabel;
+
+import root.entities.Actable;
+import root.entities.GameEntity;
+import root.entities.plants.Plant;
+import root.etc.CellsManager;
+
+public abstract class Zombie extends GameEntity implements Actable {
+
+    public static JLabel Status = new JLabel();
+    public int health;
+    public boolean specialEffect = false;
+    public boolean collided = false;
+    public boolean backward = false;
+    public int speed = 0;
+    public Effects effects;
+
+    @Override
+    public void actions() {
+        LoopCounter++;
+
+        Plant collidedPlant = collidedPlant();
+        if (collidedPlant == null) {
+            collided = false;
+            if (!specialEffect) {
+                speed = 2;
+            } else {
+                if (effects == Effects.SLOWED) {
+                    speed = 1;
+                }
+                if (effects == Effects.BACKWARD) {
+                    speed = -2;
+                }
+                if (effects == Effects.BOOST) {
+                    speed = 4;
+                }
+                if (LoopCounter % 128 == 0) {
+                    specialEffect = false;
+                }
+            }
+            position.x += dx * speed;
+            position.y += dy * speed;
+        } else {
+            collidedPlant.health -= 1;
+            collided = true;
+        }
+    }
+
+    public Zombie(int x, int y) {
+        super(x, y);
+        dx = -1;
+        dy = 0;
+    }
+
+    public static void updateStatus() {
+        String status = String.format("%d/%d", gamePanel.level.MAX_ZOMBIES - gamePanel.level.zombieKilled, gamePanel.level.MAX_ZOMBIES);
+        Status.setText(status);
+    }
+
+    public enum Effects {SLOWED, BOOST, BACKWARD}
+
+    protected Plant collidedPlant() {
+        if (!CellsManager.cellMaps.isEmpty()) {
+            for (Plant p : CellsManager.cellMaps.values()) {
+                if (Math.abs(position.y - p.getY()) <= 10) {
+                    if (Math.abs(position.x - (p.getX() + p.getImage().getWidth(null))) < Math.abs(dx + 10)) {
+                        return p;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Point getPosition() {
+        return this.position;
+    }
+}
