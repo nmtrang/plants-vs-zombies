346262d6c3654476e6ab63115fac7822a7662de8
TraLe-ITDSIU19058 <71242240+TraLe-ITDSIU19058@users.noreply.github.com>
2021-01-07 16:48:13 +0700

    Added all zombie types

---
+++ src/root/entities/zombies/NormalZombie.java
@@ -0,0 +1,23 @@
+package root.entities.zombies;
+
+import java.awt.Image;
+
+public class NormalZombie extends Zombie {
+
+    public NormalZombie(int x, int y) {
+        super(x, y);
+        health = 150;
+    }
+
+
+    @Override
+    public Image getImage() {
+        if (collided) {
+            if (health >= 60) return visualMode.NormalZombieEatImage;
+            else return visualMode.NormalZombieLostHeadEatImage;
+        } else {
+            if (health >= 60) return visualMode.NormalZombieWalkImage;
+            else return visualMode.NormalZombieLostHeadWalkImage;
+        }
+    }
+}
